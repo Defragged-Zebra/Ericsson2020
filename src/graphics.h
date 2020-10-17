77b755f50318efc416275d95938a290108b2fda4
Bence Orosz <oroszbence0929@gmail.com>
2020-10-18 00:48:43 +0200

    file structure is created empty grid is initialized

---
+++ src/graphics.h
@@ -0,0 +1,14 @@
+//
+// Created by woranhun on 2020. 10. 17..
+//
+
+#ifndef VIRUS_GRAPHICS_H
+#define VIRUS_GRAPHICS_H
+
+
+class Graphics {
+
+};
+
+
+#endif //VIRUS_GRAPHICS_H
---
+++ src/logic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by woranhun on 2020. 10. 17..
+//
+
+#include "logic.h"
