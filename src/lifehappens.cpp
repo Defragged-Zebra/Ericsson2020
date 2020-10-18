0d2dd0c9916638246afabff1edfbe3c5856d9dbf
lukacs.mark@gmail.com <vioSpark@users.noreply.github.com>
2020-10-18 19:05:24 +0200

    infection partly implemented (a bit more)

--- src/lifehappens.cpp
+++ src/lifehappens.cpp
@@ -4,8 +4,9 @@
 
 #include "lifehappens.h"
 
-int LifeHappens::calculateSpontaneousHealing(Field field, int currentTick, int healStartTick, unsigned long random1) {
-    throw std::runtime_error("under implementation");
+int LifeHappens::calculateSpontaneousHealing(Grid *grid, int fieldCoordinateX, int fieldCoordinateY, int currentTick,
+                                             int healStartTick, unsigned long random1) {
+    Field field = (*grid)[fieldCoordinateX][fieldCoordinateY];
     //healStartTick = width + height; -- it should be calculated further up for optimisation
     //currentTick = hanyadik tick van
     //Ha még nem értük el a width + height -edik kört, akkor 0
@@ -27,50 +28,66 @@ void LifeHappens::applySpontaneousHealing(int value, Field field) {
     throw error;
 }
 
-int LifeHappens::calculateSpontaneousInfection(Grid *grid, int currentTick, unsigned long random2, unsigned long random3,
+int LifeHappens::calculateSpontaneousInfection(Grid *grid, int fieldCoordinateX, int fieldCoordinateY, int currentTick,
+                                               unsigned long random2, unsigned long random3,
                                                unsigned long random4) {
+
+
     if (sectorIsClear) {
         return 0;
     } else {
         //curr_tick: ticks elapsed
         //A második véletlen faktor 10-zel való osztási maradéka + 10 darab előző vírusterjedés átlaga az adott cellán.
-        int c = min(random2 % 10 + 10, currentTick);
-        int b = avg(i =[1..c], infection(curr_tick - i, coord));
+        int intervalToAverage = min(random2 % 10 + 10, currentTick);
+        //int b = avg(i =[1..c], infection(curr_tick - i, coord));
+        Field field = (*grid)[fieldCoordinateX][fieldCoordinateY];
+
+        int b = std::accumulate(field.getLastInfectionValues()[len - intervalToAverage],
+                                field.getLastInfectionValues().end(), 0.0) /
+                field.getLastInfectionValues().len();
         int sum = 0;
-        for (c in{[coordX, coordY], [coordX-1, coordY],[coordX, coordY+1],[coordX-1, coordY],[coordX, coordY-1]}) {
-            if (/*boundary check fails*/)
-                { continue; }
-            int dist = distance(coord, c);
+        int coordinates[5][2] = {{fieldCoordinateX,     fieldCoordinateY},
+                                 {fieldCoordinateX - 1, fieldCoordinateY},
+                                 {fieldCoordinateX,     fieldCoordinateY + 1},
+                                 {fieldCoordinateX - 1, fieldCoordinateY},
+                                 {fieldCoordinateX,     fieldCoordinateY - 1}};
+        for (int i = 0; i < 5; ++i) {
+            int cX = coordinates[i][0];
+            int cY = coordinates[i][1];
+            //boundary check
+            if (cX < 0 || cY < 0 || cX > grid->getX() || cY > grid->getY()) { continue; }
+            int dist = distance(fieldCoordinateX, fieldCoordinateX, cX, cY);
             //Az átfertőződési mutatók kiszámolása előtt a t átfertőződési hajlandóságot generáljuk
             // a harmadik véletlen faktor 7-tel való osztási maradéka + 3 -mal
             int t = random3 % 7 + 3;
-            //Ehhez hozzáadva az adott cella és a szomszédjainak az átfertőződési mutatóját.
-            a = b + sum(c = [coord, neighbours(coord)];
+
+
             //Az átfertőződési mutatókat akkor adjuk össze a hely populációs különbségéből adódó,
             //1-3 érték közé beszorított fertőzési lehetőséggel,
             //ha az előző körös fertőzöttség nagyobb, mint a hajlandóság és a távolság szorzata.
             if (tick_info[curr_tick - 1, c].infection_rate /*A.K.A: previous infection rate of the cell*/ > dist * t) {
                 //"beszorított átfertőzési mutató"
-                d = std::clamp(start_info[coord].population - start_info[c].population, 0, 2) + 1;
+                int d = std::clamp(start_info[coord].population - start_info[c].population, 0, 2) + 1;
                 sum += d;
             } else {
                 continue;
             }
         }
+        //Ehhez hozzáadva az adott cella és a szomszédjainak az átfertőződési mutatóját.
+        int a = b + sum;
         //Az így eddig kiszámolt összeget megszorozzuk a negyedik véletlen faktor 25-tel való osztási maradéka + 50-nel,
         //és az egészet leosztjuk 100-al, majd vesszük a felső egészrészét.
-        return ceil(a * (factor4_next() % 25 + 50) / 100.0);
+        return ceil(a * (random4 % 25 + 50) / 100.0);
     }
 }
+
 int LifeHappens::distance(coord, c) {
     //A távolság helyben 0, megegyező kerületben 1, egyébként 2
-    if (c.district != coord.district)
-        {return 2;}
-    else{
-        if (coord==c){
+    if (c.district != coord.district) { return 2; }
+    else {
+        if (coord == c) {
             return 0;
-        }
-        else{
+        } else {
             return 1;
         }
     }
