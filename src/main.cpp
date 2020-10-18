750b5bcfba2fbe9e300e05129e74eccde5341ff9
Bence Orosz <35062978+woranhun@users.noreply.github.com>
2020-10-18 03:07:02 +0200

    cleaning code + writing basic stuffs (#1)

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "antiVirus.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    AntiVirus av = AntiVirus(10,10);
+    AntiVirus av = AntiVirus(10,10,0,5);
     av.run();
     std::cout<<av;
     return 0;
