06e08fddcdf1461d198b0d5b204adfbdf50ae5a4
Bence Orosz <oroszbence0929@gmail.com>
2020-10-30 17:17:59 +0100

    bugfix: round changed to floor

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -19,34 +19,35 @@ void Protocol::request(std::string& line){
     ss<<line;
     int gameID,tickID,countryID;
     ss>>tmp>>gameID>>tickID>>countryID;
-    ers<<"Factors before simulation: ";
-    for (int i = 1; i <= 4; ++i) {
-        ers<<grid->random.getFactor(i)<<" ";
-    }
-    ers<<std::endl;
+    //ers<<"Factors before simulation: ";
+    //for (int i = 1; i <= 4; ++i) {
+    //    ers<<grid->random.getFactor(i)<<" ";
+   // }
+    //ers<<std::endl;
     //if (grid->getCurrentTick()==0){Logic::shiftFactor2to4(); }
     Logic::simulateTO(gameID,tickID,countryID);
 
     Protocol::currentResult(gameID,tickID,countryID);
-    ers<<std::endl<<"Factors after simulation: ";
-    for (int i = 1; i <= 4; ++i) {
-        ers<<grid->random.getFactor(i)<<" ";
-    }
-    ers<<std::endl;
-    ers<<"-----------------------"<<std::endl;
+    //ers<<std::endl<<"Factors after simulation: ";
+    //for (int i = 1; i <= 4; ++i) {
+        //ers<<grid->random.getFactor(i)<<" ";
+    //}
+    //ers<<std::endl;
+    //ers<<"-----------------------"<<std::endl;
 }
 void Protocol::currentResult(int gameID,int tickID,int countryID) {
     os << "RES " << gameID <<" " << tickID <<" "<< countryID <<std::endl;
     for (size_t y = 0; y < grid->getY(); ++y) {
         for (size_t x = 0; x < grid->getX() ; ++x) {
             os << grid->getFieldByID((*grid)[y][x]).getCurrentInfectionRate() << " ";
+            //if(grid->getCurrentTick()==25&&y==4&&x==6)std::cerr<<"Curr rate: "<<grid->getFieldByID((*grid)[y][x]).getCurrentInfectionRate()<<std::endl;
         }
         os<<std::endl;
     }
     os << "." <<std::endl;
 }
 void Protocol::initAntiVirus() {
-    init();
+    init(4);
     initValues();
 }
 void Protocol::initValues() {
