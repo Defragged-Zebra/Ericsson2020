6cc9aa5ecab60852a241c9c24e9476abdd14ea9b
lukacs.mark@gmail.com <vioSpark@users.noreply.github.com>
2020-11-06 21:56:57 +0100

    Errors cleared after Bence

--- src/iface.cpp
+++ src/iface.cpp
@@ -79,7 +79,7 @@ void Iface::createGrid() {
 void Iface::start() {
     std::string line;
     while (std::getline(is, line)) {
-        if (line == "." or line.empty()) {
+        if (line == ".\r" or line == "\r") {
             continue;
         } else if (line == "SUCCESS") {
             Iface::sendDebugMsg("SUCCESS");
@@ -90,6 +90,7 @@ void Iface::start() {
         } else if (line.find("FAILED") != std::string::npos) {
             Iface::sendDebugMsg(line);
             break;
+            //this else is never reached
         } else if (line.find("WARN") != std::string::npos) {
             Iface::sendDebugMsg(line);
             throw std::runtime_error("Beszoptuk a faszt!!444!!!");
@@ -97,7 +98,7 @@ void Iface::start() {
             Iface::round(line);
         } else {
             Iface::sendDebugMsg("Miafasz történt?");
-            Iface::sendDebugMsg("[DEBUG] Hibát okozta\""+line+"\" [DEBUG VEGE]");
+            Iface::sendDebugMsg("[DEBUG] Hibát okozta\"" + line + "\" [DEBUG VEGE]");
             throw std::runtime_error("Miafasz történt?");
         }
     }
@@ -122,19 +123,24 @@ void Iface::round(std::string &line) {
     ss >> tmp >> _gameID >> tickID >> countryID;
 
     //Process input values
-    tmp="";
-    while(std::getline(is,tmp)){
-        Iface::sendDebugMsg("[NOTIFY] "+tmp);
-        if(tmp!=".")continue;
+    tmp = "";
+    while (std::getline(is, tmp)) {
+        Iface::sendDebugMsg("[NOTIFY] " + tmp);
+        if (tmp.find("WARN") != std::string::npos) {
+            Iface::sendDebugMsg(line);
+            throw std::runtime_error("We've fucked it up!!444!!!");
+        }
+        if (tmp != ".\r")continue;
         else break;
     }
+
 
     Logic::simulateTO(_gameID, tickID, countryID);
 
     //Send result back
     os << "RES " << _gameID << " " << tickID << " " << countryID << std::endl;
     std::vector<VaccineData> back;// Ha hozzá nyúlsz letöröm a kezed!!!!
-    back= Logic::calculateBackVaccines(back, tickID);
+    back = Logic::calculateBackVaccines(back, tickID);
     for (auto &i : back) {
         os << "BACK " << i.getY() << " " << i.getX() << " " << i.getVaccines() << std::endl;
     }
