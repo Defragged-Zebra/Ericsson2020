491b5858a855c41e30378f58030d94500fa773e2
vioSpark <40240957+vioSpark@users.noreply.github.com>
2020-11-06 22:11:17 +0100

    Dev round2 testfiles (#36)
    * test files for round2 made
    
    * Errors cleared after Bence
    
    Co-authored-by: lukacs.mark@gmail.com <vioSpark@users.noreply.github.com>

--- src/logic.cpp
+++ src/logic.cpp
@@ -189,7 +189,7 @@ std::vector<VaccineData> &Logic::calculateBackVaccines(std::vector<VaccineData> 
 std::vector<VaccineData> &Logic::calculatePutVaccines(std::vector<VaccineData> &put, size_t tickID) {
     //TODO make this
     return put;
-
+}
 double Logic::calculateCrossInfection(const Point &center, uint64_t factor3) {
     Field &field = grid->getFieldByPoint(center);
     double sum = 0;
